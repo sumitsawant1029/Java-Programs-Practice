43b8742eba75db223fcdc095eeb3401bede31826
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-09-01 02:00:15 -0700

    Added Class One for Static Another for Non Static

---
+++ StaticN.java
@@ -0,0 +1,12 @@
+class C{
+    public static print(){
+        System.out.println(" Static Funtion is called");
+
+    }
+}
+class B{
+    public print(){
+        System.out.println(" Non Static Variable is Called");
+    }
+
+}
