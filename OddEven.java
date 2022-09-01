c02e6967eaa6a8d61790fcb287af08de0555f960
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-09-01 03:20:17 -0700

    Added comments

--- OddEven.java
+++ OddEven.java
@@ -6,17 +6,18 @@ class A1{
         System.out.println(" Checking Odd And Even ");
     }
     public static int Checking(int x){
-            if(x%2==0)
+            if(x%2==0)// if the number is even its remainder is 0
                 return 1;
             else 
                 return 0;
     }
 }
 class OddEven{
-    private static Scanner sc;
+    private static Scanner sc;// To Take inputs 
     public static void main(String args[]){
         
         sc = new Scanner(System.in);
+        // Creted an object for taking input 
         System.out.println("Enter The nUmber : ");
         int x =sc.nextInt();
         int y=A1.Checking(x);
