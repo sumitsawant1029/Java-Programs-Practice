424a994a5e3d6b92f567d46192c96ad29107c3b7
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-09-10 04:57:13 -0700

    Added OCBR

---
+++ Box/pass.java
@@ -0,0 +1,23 @@
+// Object Called By there Reference 
+
+class OCBR{
+    int a,b;
+    OCBR(int i, int j){
+        a=i;
+        b=j;
+    }
+
+    void meth(OCBR o){
+        o.a*= 2;
+        o.b /= 2;
+    }
+}
+public class pass{
+    public static void main(String args[]){
+        OCBR ob = new OCBR(12,22);
+        System.out.println(""+ob.a+""+ob.b);
+        ob.meth(ob);
+        System.out.println(""+ob.a+""+ob.b);
+
+    }
+}
