6fd1f92d7eccfadf52125749dec95f9c0298a05d
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-09-10 04:36:19 -0700

    Update the code

--- Box/Box.java
+++ Box/Box.java
@@ -1,10 +1,14 @@
-Box(){
+class Box{
+    double width,breath,length;
+
     Box(){
         System.out.println("Constructing Box");
+        width=10;
+        breath=10;
+        length=10;
     }
     public void Area(double w,double h, double b){
-        this.width=w;
-        this.height=h;
-        this.breath=b;
+        System.out.println(""+w);
+
     }
 }
---
+++ Box/Box1.java
@@ -0,0 +1,7 @@
+public class Box1{
+    public static void main(String args[]){
+        Box obj= new Box();
+        obj.Area(12,22,22);
+
+    }
+}
