9f1aa26b677d3d52fcafef668ca5d4ad5877622e
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-09-10 04:29:16 -0700

    Update

--- Box/Box.java
+++ Box/Box.java
@@ -3,7 +3,8 @@ Box(){
         System.out.println("Constructing Box");
     }
     public void Area(double w,double h, double b){
-        
-
+        this.width=w;
+        this.height=h;
+        this.breath=b;
     }
 }
